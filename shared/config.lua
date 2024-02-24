6cf2a497a55ef7bcb52c82970783842d09b0dcbd
Poseidon <62171904+Poseidon281@users.noreply.github.com>
2024-02-24 12:23:48 +0100

    Create config.lua

---
+++ shared/config.lua
@@ -0,0 +1,12 @@
+Discord = {
+
+    warns = "https://discord.com/api/webhooks/",
+    heal = "https://discord.com/api/webhooks/",
+    mededeling = "https://discord.com/api/webhooks/",
+    ban = "https://discord.com/api/webhooks/",
+    whitelist = "https://discord.com/api/webhooks/",
+    acties = "https://discord.com/api/webhooks/",
+    kick = "https://discord.com/api/webhooks/",
+    restart = "https://discord.com/api/webhooks/", 
+
+}
